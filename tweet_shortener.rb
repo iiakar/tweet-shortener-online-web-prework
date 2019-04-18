d8ab312d1fb81676afbc6c75cbc0474cbe160ec0
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-18 18:48:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
-def word_substituter' do
-    it 'replaces long words with their expected short form' do
-      expect(word_substituter(tweet_one)).to eq(tweet_one_short)
-    end
+def word_substituter 
+  do
+   word_substituter(tweet_one)).to eq(tweet_one_short)
+  
   end
