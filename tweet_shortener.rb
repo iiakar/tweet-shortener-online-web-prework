c433dd746341d331a152fc7b45a803f17a150143
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-18 18:51:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,6 @@
 def word_substituter 
-  do
-   word_substituter(tweet_one)).to eq(tweet_one_short)
   
-  let(:tweet_one)   {"Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"}
+  if  word_substituter >140 (:tweet_one).let(:tweet_one_short)
+   
+ end
   
-  let(:tweet_one_short)   {"Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"}
-  
-  
-  end
