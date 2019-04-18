e1d08b0c22d12e74057e5350ec8a361a6409ab1f
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-18 18:49:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,5 +4,7 @@ def word_substituter
   
   let(:tweet_one)   {"Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"}
   
+  let(:tweet_one_short)   {"Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"}
+  
   
   end
